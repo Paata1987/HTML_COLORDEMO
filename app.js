ca57c244fbb3a43819713a0dc8b4bfb8dd93eee7
Paata Home <paata.tepnadze@gmail.com>
2023-01-18 12:52:48 +0100

    add text color  convertation by shades

--- app.js
+++ app.js
@@ -10,12 +10,19 @@ function randomRGBColorGenerator() {
   return '#' + singleColor;
 }
 
-function setRandomColor() {}
+function setRandomColors() {}
 columns.forEach((el) => {
-  const Text = el.querySelector('h2');
+  const text = el.querySelector('h2');
   const color = chroma.random();
   Text.textContent = color;
   el.style.background = color;
-  //console.log(col);
+  setTextColor(text, color);
 });
-setRandomColor();
+setRandomColors();
+
+function setTextColor(text, color) {
+  const luminance = chroma(color).luminance();
+  text.style.color = luminance > 0.5 ? 'black' : 'white';
+}
+
+setRandomColors();

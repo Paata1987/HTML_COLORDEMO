cec7ef9cc2a6ebf018b2cafc83d209ad06999b67
Paata Home <paata.tepnadze@gmail.com>
2023-01-18 10:20:55 +0100

    add js file

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 const columns = document.querySelectorAll('.col');
 
+function randomRGBColorGenerator() {
+  const rgbHexColorCodes = '0123456789ABCDEF';
+  let singleColor = '';
+  for (let iterator = 0; iterator < 6; iterator++) {
+    singleColor +=
+      rgbHexColorCodes[Math.floor(Math.random() * rgbHexColorCodes.length)];
+  }
+  return '#' + singleColor;
+}
+
 function setRandomColor() {}
 columns.forEach((el) => {
   console.log(el);
